80efdf151b7a43807551fc6b63e4f172af15e41f
Jacqui23 <jacquelynedward98@gmail.com>
2023-05-19 12:43:49 +0200

    ABC

---
+++ Second ViewControllwer.swift
@@ -0,0 +1,29 @@
+//
+//  Second ViewControllwer.swift
+//  ABC
+//
+//  Created by DA_MAC_104 on 2023/05/19.
+//
+
+import UIKit
+
+class Second_ViewControllwer: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
